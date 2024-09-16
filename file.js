f99bad52ece48e48f439b2032d98eac32c23f91a
amrit2599 <107432436+amrit2599@users.noreply.github.com>
2024-09-16 18:29:24 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,30 @@
+//1.Write a JavaScript program that displays the largest integer among two integers
+
+// let a = prompt("write your number a");
+// let b = prompt("write your number b");
+
+// if (parseInt(a) > parseInt(b)) {
+//   console.log(`${a} is the largest intiger`);
+// } else if (parseInt(b) > parseInt(a)) {
+//   console.log(`${b} is the largest intiger`);
+// } else {
+//   console.log("both numbers  are equal");
+// }
+
+//2.Write a JavaScript conditional statement to find the sign of the product of three numbers. Display an alert box with the specified sign.
+//Sample numbers : 3, -7, 2
+//Output : The sign is -
+
+// let a = -3;
+// let b = 7;
+// let c = -2;
+
+// if (a * b * c >= 0) {
+//   console.log(alert(`The sign is +`));
+// } else {
+//   console.log(alert(`The sign is -`));
+// }
+
+//3.Write a JavaScript conditional statement to sort three numbers. Display an alert box to show the results.
+//Sample numbers : 0, -1, 4
+//Output : 4, 0, -1
